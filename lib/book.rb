df56bf6cdef4b2209bac652803aa70215e12c9d7
Tristan Brown <tgbrown@stanford.edu>
2018-09-01 05:14:25 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,17 @@
 class Book
 
   def initialize(title)
-    @title = title 
+    @title = title
   end
 
   def title
+    @title
   end
+
+  def author=(author)
+    @author = author
+  end
+
+  def author
+    @author
 end
